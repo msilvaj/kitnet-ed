f3e2ed50a2ec210f168fe2a9ca4049e3a943faec
msilvaj <miqueias.ufpi@gmailcom>
2019-06-09 13:53:42 -0300

    Mais configurações de layout

--- app/controllers/inquilinos_controller.rb
+++ app/controllers/inquilinos_controller.rb
@@ -1,12 +1,23 @@
 class InquilinosController < ApplicationController
   before_action :set_inquilino, only: [:show, :edit, :update, :destroy]
+  #before_action :calcula_pagamento, only: [:show, :edit, :update, :destroy]
+
 
   # GET /inquilinos
   # GET /inquilinos.json
   def index
     @inquilinos = Inquilino.order(:dataVencimento)
-
+    
   end
+
+
+  def calcula_pagamento
+    @ob = Inquilino.all.each { |name| x }
+    if @x.dataVencimento.day.eql? Date.today.day
+      @x.pago = false
+    end
+  end
+
 
   # GET /inquilinos/1
   # GET /inquilinos/1.json
@@ -30,11 +41,11 @@ class InquilinosController < ApplicationController
 
     respond_to do |format|
       if @inquilino.save
-        format.html { redirect_to @inquilino, notice: 'Inquilino was successfully created.' }
-        format.json { render :show, status: :created, location: @inquilino }
+        format.html {redirect_to @inquilino, notice: 'Inquilino was successfully created.'}
+        format.json {render :show, status: :created, location: @inquilino}
       else
-        format.html { render :new }
-        format.json { render json: @inquilino.errors, status: :unprocessable_entity }
+        format.html {render :new}
+        format.json {render json: @inquilino.errors, status: :unprocessable_entity}
       end
     end
   end
@@ -44,11 +55,11 @@ class InquilinosController < ApplicationController
   def update
     respond_to do |format|
       if @inquilino.update(inquilino_params)
-        format.html { redirect_to @inquilino, notice: 'Inquilino was successfully updated.' }
-        format.json { render :show, status: :ok, location: @inquilino }
+        format.html {redirect_to @inquilino, notice: 'Inquilino was successfully updated.'}
+        format.json {render :show, status: :ok, location: @inquilino}
       else
-        format.html { render :edit }
-        format.json { render json: @inquilino.errors, status: :unprocessable_entity }
+        format.html {render :edit}
+        format.json {render json: @inquilino.errors, status: :unprocessable_entity}
       end
     end
   end
@@ -58,19 +69,20 @@ class InquilinosController < ApplicationController
   def destroy
     @inquilino.destroy
     respond_to do |format|
-      format.html { redirect_to inquilinos_url, notice: 'Inquilino was successfully destroyed.' }
-      format.json { head :no_content }
+      format.html {redirect_to inquilinos_url, notice: 'Inquilino was successfully destroyed.'}
+      format.json {head :no_content}
     end
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_inquilino
-      @inquilino = Inquilino.find(params[:id])
-    end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def inquilino_params
-      params.require(:inquilino).permit(:nome, :cpf, :rg, :telefone, :ap, :codigoEletrobras, :dataInicio, :dataFim, :dataVencimento, :pago)
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_inquilino
+    @inquilino = Inquilino.find(params[:id])
+  end
+
+  # Never trust parameters from the scary internet, only allow the white list through.
+  def inquilino_params
+    params.require(:inquilino).permit(:nome, :cpf, :rg, :telefone, :ap, :codigoEletrobras, :dataInicio, :dataFim, :dataVencimento, :pago)
+  end
 end
